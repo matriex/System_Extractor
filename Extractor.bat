d6d045ae48c749d34a3cb166a6b52154bea2286e
Leo <Leo3418@users.noreply.github.com>
2017-01-25 20:53:58 +0800

    Remove redundant ".zip" suffix of signed ZIPs
    When a ZIP file is being signed, the signed package will have an additional ".zip" suffix.
    This change removes that additional file extension name.

--- Extractor.bat
+++ Extractor.bat
@@ -506,7 +506,7 @@ cls
 
   cd sign_files
 
-java -jar signapk.jar testkey.x509.pem testkey.pk8 %filename% signed_%filename%.zip 
+java -jar signapk.jar testkey.x509.pem testkey.pk8 %filename% signed_%filename%
   del signapk.jar
   del testkey.x509.pem
   del testkey.pk8
